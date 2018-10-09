6a57f8b9eb9c9ab7431f25c7abd5a175519af87b
Stephen Caldwell <steve@campusexplorer.com>
2018-10-09 14:48:43 -0700

    feat: SnapshotRepositoryS3 class, spec test

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
-class Script {
-    constructor(config) {
-        config = { ...config };
-        this._name = config.name || 'script';
+const autoBind = require('auto-bind');
+const amazonS3URI = require('amazon-s3-uri');
 
-        this.renderName = this.renderName.bind(this);
+class SnapshotRepositoryS3 {
+    constructor({ s3Client, s3Path }) {
+        this.s3Client = s3Client;
+        this.s3Path = s3Path;
+        const { bucket, key } = amazonS3URI(s3Path);
+        this.s3Bucket = bucket;
+        this.s3KeyPrefix = key.replace(/\/$/, '');
+        autoBind(this);
     }
 
-    renderName() {
-        return this._name;
+    getS3Location(aggregateName, aggregateId) {
+        return {
+            bucket: this.s3Bucket,
+            key: `${this.s3KeyPrefix}/${aggregateName}/${aggregateId}.json`,
+        };
+    }
+
+    async getSnapshot(aggregateName, aggregateId) {
+        const { bucket, key } = this.getS3Location(aggregateName, aggregateId);
+        const response = await this.s3Client
+            .getObject({
+                Bucket: bucket,
+                Key: key,
+            })
+            .promise();
+        const { Body: body } = response;
+        return body ? JSON.parse(body) : undefined;
+    }
+
+    async writeSnapshot(aggregateName, aggregateId, snapshot) {
+        const { bucket, key } = this.getS3Location(aggregateName, aggregateId);
+        await this.s3Client
+            .putObject({
+                Bucket: bucket,
+                Key: key,
+                Body: JSON.stringify(snapshot),
+            })
+            .promise();
+        return true;
     }
 }
 
-module.exports = Script;
+module.exports = SnapshotRepositoryS3;
